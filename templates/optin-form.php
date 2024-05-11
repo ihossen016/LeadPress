6b3ab328b9dd8de68a6e5bf3e3e9a1daed5feb04
ihossen016 <ihossen016@gmail.com>
2024-05-11 11:00:36 +0600

    #1: thankyou msg added

--- templates/optin-form.php
+++ templates/optin-form.php
@@ -15,3 +15,12 @@
         <?php _e( 'Subscribe', 'leadpress' ); ?>
     </button>
 </form>
+
+<div class="leadpress-optin-message hide">
+    <h3><?php _e( 'Thank you for Subscribing', 'leadpress' ); ?></h3>
+    <div class="leadpress-optin-message-icon">
+        <img src="<?php echo esc_url( LEADPRESS_ASSET . '/img/check.png' ); ?>" alt="check">
+    </div>
+    <p><?php _e( 'You have been subscribed to our mailing list', 'leadpress' ); ?></p>
+    <p><?php _e( 'You will receive an email shortly', 'leadpress' ); ?></p>
+</div>
