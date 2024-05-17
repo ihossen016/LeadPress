2bfbbcac20963c767d60736b21b76e4183c494d3
ihossen016 <ihossen016@gmail.com>
2024-05-17 22:05:09 +0600

    vendor removed

--- templates/menus/email-logs.php
+++ templates/menus/email-logs.php
@@ -1,6 +1,15 @@
 <?php
 
 use Ismail\LeadPress\Email\EmailLogs;
+use Ismail\LeadPress\Utils\Table;
+
+$columns = [
+    'col_id'      	=> __( 'ID', 'leadpress' ),
+    'col_name'    	=> __( 'Name', 'leadpress' ),
+    'col_email'   	=> __( 'Email', 'leadpress' ),
+    'col_status'  	=> __( 'Status', 'leadpress' ),
+    'col_date'   	=> __( 'Date', 'leadpress' ),
+];
 
 $logs = new EmailLogs();
 
