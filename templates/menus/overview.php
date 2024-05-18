e2d9331dd3899ff70f0b3eb1cb51624b0cd216fd
ihossen016 <ihossen016@gmail.com>
2024-05-18 23:23:48 +0600

    overview design create

--- templates/menus/overview.php
+++ templates/menus/overview.php
@@ -3,4 +3,10 @@ use Ismail\LeadPress\Public\Helper;
 
 ?>
 
-<h1>LeadPress Overview</h1>
+<div class="leadpress-overview-wrapper">
+    <div class="leadpress-overview">
+        <h1><?php _e('Welcome', 'leadpress'); ?></h1>
+        <h1><?php _e('to', 'leadpress'); ?></h1>
+        <h1><?php _e('LeadPress', 'leadpress'); ?></h1>
+    </div>
+</div>
