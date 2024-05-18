bcdd87da70e6d299b592d4313dbf4ce620d3c722
ihossen016 <ihossen016@gmail.com>
2024-05-18 23:36:36 +0600

    uninstall functions create

--- uninstall.php
+++ uninstall.php
@@ -1,1 +1,12 @@
 <?php
+// If uninstall is not called, exit
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+    exit;
+}
+
+// delete cache
+delete_leadpress_cache( 'leads' );
+delete_leadpress_cache( 'email_logs' );
+
+// delete scheduled emails
+as_unschedule_all_actions( 'leadpress_schedule_email' );
